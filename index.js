087671a38d637984887c87284251af72bbcd747a
Katricia Smith <smithkatricia@gmail.com>
2018-11-16 15:44:35 -0800

    Add initial clock js code

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Your code here
+$(document).ready(function() {
+
+ const clock = new Date;
+
+ let display = $('<p>' + clock + '</p>');
+
+
+// console.log(clock);
+$('#clock').append(display);
+
+
+
+
+
+
+})
