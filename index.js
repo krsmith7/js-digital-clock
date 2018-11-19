c83f29877988b890a96149c31f45a3cac56099fe
Katricia Smith <smithkatricia@gmail.com>
2018-11-19 14:38:08 -0800

    Add buttons to align clock left or right

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 $(document).ready(function() {
 
 setInterval(function () {
-
   const clock = (new Date).toLocaleTimeString();
 
   let display = $('<p>' + clock + '</p>');
-
 
   $('#clock').html(display);
 
 }, 500);
 
+$('#left-button').click( function() {
+  let target = $('#clock');
+  console.log('left align?');
+  target.removeClass();
+  target.addClass('left-align')
+});
 
+$('#right-button').click( function() {
+    let target = $('#clock');
+    console.log('right align?');
+  target.removeClass();
+  target.addClass('right-align')
+});
 
 
 });
